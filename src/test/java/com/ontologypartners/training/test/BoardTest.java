9b2711e2c9477a45f2fbcecf6327d26b2f3e88e6
Pablo Bayarri <pablo.bayarri@exfo.com>
2019-11-29 10:32:19 +0100

    starting scenario

--- src/test/java/com/ontologypartners/training/test/BoardTest.java
+++ src/test/java/com/ontologypartners/training/test/BoardTest.java
@@ -17,7 +17,7 @@ public class BoardTest {
 		assertTrue(board.canMove(playerAtHome, 5));
 	}
 	@Test
-	public void NoFiveScoreCanotnMovePlayerAtHome() {
+	public void NoFiveScoreCannotMovePlayerAtHome() {
 		Board board = new Board();
 		assertFalse(board.canMove(playerAtHome, 3));
 	}
